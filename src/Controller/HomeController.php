499c90c66f1d17607a884af2c8f44304a4c90ba2
Nat <nathalie.toublanc@viacesi.fr>
2019-12-15 06:05:50 +0100

    Templates first Level
    About, school, hireus. Nav and footer.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -24,4 +24,52 @@ class HomeController
         $content = $twig->render('pages/home.html.twig');
         return new Response($content);
     }
+
+    /**
+ * @Route("/about", name="about")
+ * @param Environment $twig
+ * @return Response
+ * @throws \Twig\Error\LoaderError
+ * @throws \Twig\Error\RuntimeError
+ * @throws \Twig\Error\SyntaxError
+ */
+    public function about(Environment $twig)
+    {
+        // Initialisation de l'objet twig
+        $content = $twig->render('pages/about.html.twig');
+        return new Response($content);
+
+    }
+
+    /**
+     * @Route("/ecole", name="school")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function school(Environment $twig)
+    {
+        // Initialisation de l'objet twig
+        $content = $twig->render('pages/school.html.twig');
+        return new Response($content);
+
+    }
+
+    /**
+     * @Route("/prestation", name="hireus")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function hireus(Environment $twig)
+    {
+        // Initialisation de l'objet twig
+        $content = $twig->render('pages/hireus.html.twig');
+        return new Response($content);
+
+    }
 }
